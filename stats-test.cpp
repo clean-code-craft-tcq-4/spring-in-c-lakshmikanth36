83680d78b780931fbc9e3f71bef99ec2b8ddc310
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-05-19 23:06:15 +0530

    Update stats-test.cpp
    Test the code

--- stats-test.cpp
+++ stats-test.cpp
@@ -6,11 +6,12 @@
 #include <stdlib.h>
 #include <math.h>
 
+float numberset[] = {1.5, 8.9, 3.2, 4.5};
+int setlength = sizeof(numberset) / sizeof(numberset[0]);
+struct Stats computedStats = compute_statistics(numberset, setlength);
+float epsilon = 0.001;
+
 TEST_CASE("reports average, minimum and maximum") {
-    float numberset[] = {1.5, 8.9, 3.2, 4.5};
-    int setlength = sizeof(numberset) / sizeof(numberset[0]);
-    struct Stats computedStats = compute_statistics(numberset, setlength);
-    float epsilon = 0.001;
     REQUIRE(abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(abs(computedStats.min - 1.5) < epsilon);
