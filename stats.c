2ecd20382da528cbebd6f6c267b6105db0c33861
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-05-20 21:02:45 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -5,6 +5,7 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    float sum=0;
     for (int i=0;i<setlength;i++)
     {
         sum =sum+numberset[i];
