319b39f3c38b7b8d66ad99c63932c99e2f9a3f21
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-05-20 16:43:59 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,10 +1,16 @@
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
+    int n;
     struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    for (int i = 0; i < setlength; ++i) {
+        printf("%d. Enter number: ", i + 1);
+    sum += num[i];
+    avg = sum / n;
+    printf("Average = %.2f", avg);
     return s;
 }
 
