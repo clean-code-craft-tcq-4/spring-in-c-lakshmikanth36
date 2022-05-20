356d944cfc93b0d6f1c84aa07031212a1ebef93c
Lakshmikanth Routhu <lakshmikanthr36@gmail.com>
2022-05-20 16:54:02 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -1,15 +1,10 @@
 #include "stats.h"
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
-    int n,sum;
-    float avg;
     struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
-    for (int i = 0; i < setlength; ++i) {
-    sum += num[i];
-    s.average = sum / n;
     return s;
 }
 
